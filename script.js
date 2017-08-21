4893266315817b6003e3009c3b6ddddef010b1d8
fajarkarim <fajarabdulkarim@gmail.com>
2017-08-21 20:04:23 +0700

    draw words done

--- script.js
+++ script.js
@@ -21,77 +21,63 @@ let reload = () => {
     rows.forEach(row => {
       goalsScored.push(+row.GoalsScored)
     })
-
-    const yScale = d3.scaleLinear()
-    .domain([0, d3.max(goalsScored)])
-    .range([0, height])
-
-    const colorScale = d3.scaleLinear()
-    .domain([0, d3.max(goalsScored)])
-    .range(['peru', 'teal'])
-
-    var xAxis = d3.scaleLinear()
-                  .domain([0, goalsScored.length])
-                  .range([0, width])
-
-    var yAxis = d3.scaleLinear()
-                  .domain([0, d3.max(goalsScored)])
-                  .range([height - margin, 0])
-
-
-    var barWidth = width/goalsScored.length
-
-    svg.selectAll('rect')
-      .data(goalsScored)
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', (d, i) => {
-        return i * barWidth + marginLeft - 10
-      })
-      .attr('y', d => height - margin - yScale(d))
-      .attr('width', 15)
-      .attr('height', d => yScale(d))
-      .attr('fill', colorScale)
-
-      svg.append('g')
-         .attr('class', 'axisSteelBlue')
-         .attr('transform', `translate(${marginLeft - 10})`)
-         .call(d3.axisLeft(yAxis).ticks(d3.max(goalsScored)))
-
-      svg.append('g')
-         .attr('class', 'axisSteelBlue')
-         .attr('transform', `translate(${marginLeft - 10}, ${height - margin})`)
-         .call(d3.axisBottom(xAxis).ticks(goalsScored.length))
-
-      d3.selectAll('rect')
-      .attr('height', 0)
-        .transition()
-        .duration(200)
-        .delay((d, i) => i * 100)
-        .attr('y', (d, i) => height - margin - yScale(d))
-        .attr('height', (d, i) => yScale(d))
-
-
-      // svg.append('g')
-      //   .attr('class', 'axisBlue')
-      //   .attr('transfrom', `translateX(${marginLeft}, ${height - margin})`)
-      //   .call(d3.axisBottom(xAxis).ticks(goalsScored.length))
-
-      // svg.append('g')
-      //   .attr('class', 'axisBlue')
-      //   .attr('transfrom', `translateY(${marginLeft - 39})`)
-      //   .call(d3.axisLeft(yAxis).ticks(d.max(goalsScored)))
+    redraw(goalsScored)
   })
 
 }
 
 // redraw function
-let redraw = (data) => {
-  // Your data to graph here
-  // console.log(reload())
+let redraw = (goalsScored) => {
+  const yScale = d3.scaleLinear()
+  .domain([0, d3.max(goalsScored)])
+  .range([0, height])
+
+  const colorScale = d3.scaleLinear()
+  .domain([0, d3.max(goalsScored)])
+  .range(['peru', 'teal'])
+
+  var xAxis = d3.scaleLinear()
+                .domain([0, goalsScored.length])
+                .range([0, width])
+
+  var yAxis = d3.scaleLinear()
+                .domain([0, d3.max(goalsScored)])
+                .range([height - margin, 0])
+
+
+  var barWidth = width/goalsScored.length
+
+  svg.selectAll('rect')
+    .data(goalsScored)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => {
+      return i * barWidth + marginLeft - 10
+    })
+    .attr('y', d => height - margin - yScale(d))
+    .attr('width', 15)
+    .attr('height', d => yScale(d))
+    .attr('fill', colorScale)
+
+    svg.append('g')
+       .attr('class', 'axisSteelBlue')
+       .attr('transform', `translate(${marginLeft - 10})`)
+       .call(d3.axisLeft(yAxis).ticks(d3.max(goalsScored)))
+
+    svg.append('g')
+       .attr('class', 'axisSteelBlue')
+       .attr('transform', `translate(${marginLeft - 10}, ${height - margin})`)
+       .call(d3.axisBottom(xAxis).ticks(goalsScored.length))
+
+    d3.selectAll('rect')
+    .attr('height', 0)
+      .transition()
+      .duration(200)
+      .delay((d, i) => i * 100)
+      .attr('y', (d, i) => height - margin - yScale(d))
+      .attr('height', (d, i) => yScale(d))
 
 }
 
-// console.log(reload())
 reload()
--- topscore.js
+++ topscore.js
@@ -6,13 +6,45 @@ let leaderScale = d3.scaleLinear()
 
 
 const draw = (words) => {
-  // Draw your data here...
 
+  d3.select('#top-score').append('svg')
+    .attr('width', 750)
+    .attr('height', 300)
+    .attr('class', 'wordcloud')
+    .append('g')
+    .attr('transform', 'translate(320, 200)')
+    .selectAll('text')
+    .data(words)
+    .enter()
+    .append('text')
+    .style('font-size', d => d.size + 'px')
+    .style('fill', (d, i) => fill(i))
+    .attr('transform', d => {
+      return `translate(${[d.x, d.y]})rotate(${d.rotate})`
+    })
+    .text(d => d.text)
 }
 
 
 const load = () => {
   // Load your data here...
+  let names = []
+  d3.tsv('stats.tsv', rows => {
+    var players = rows
+      .filter(function (d) { return +d.G > 0 })
+      .map(function (d) { return { text: d.Name, size: +d.G }})
+      .sort(function (a, b) { return d3.descending(a.size, b.size)})
+      .slice(0, 100)
+    
+    d3.layout.cloud().size([width, height])
+      .words(players)
+      .padding(5)
+      .rotate(function () { return ~~(Math.random() * 2) * 90})
+      .font("Roboto")
+      .fontSize(function (d) { return d.size })
+      .on("end", draw)
+      .start()
+  })
 }
 
 load()
